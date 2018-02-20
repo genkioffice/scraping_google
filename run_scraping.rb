454561693705105fc49ba3778e585f132e1db565
Genki Takahashi <genki.takahashi.contact@gmail.com>
2018-02-20 09:43:30 +0900

    create two functions in scrape_with_things and can be called in run_scraping.rb

--- run_scraping.rb
+++ run_scraping.rb
@@ -1,1 +1,11 @@
-load 'scrape_review_on_google.rb'
+# load 'scrape_review_on_google.rb'
+require './scrape_with_things'
+
+print('enter url')
+# url = gets.chomp
+url_with = 'https://play.google.com/store/apps/details?id=vg.cakejam.TreasureHuntCleopatra'
+url_without = 'https://play.google.com/store/apps/details?id=jp.gungho.padRadar'
+a = check_add(url)
+c = 10
+puts(c)
+puts(c + a)
